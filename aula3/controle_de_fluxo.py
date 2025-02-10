d1e6d242e8f7e7eb33b162ef9683d42322e99f7c
Junior <jacsonsajr.study@gmail.com>
2025-02-10 16:33:59 -0300

    add laços de repetição

---
+++ aula3/controle_de_fluxo.py
@@ -0,0 +1,43 @@
+
+#MODELO1
+#qtd= 40
+#vlr= 30
+#if qtd > 0 and vlr > 0:
+    #print('valores validos')
+#else:
+    #print('valores invalidos')    
+
+#MODELO2
+#for i in range(2,16,2):
+    #print(i)
+
+#MODELO3
+#lista_alunos= ['jose', 'gustavo', 'junior']
+#for aluno in lista_alunos:
+    #print(lista_alunos)
+
+
+#texto= 'hoje é nossa segunda aula do bootcamp, bootcamp de python'
+
+#novo_texto= texto.replace(',', '') # primeiro fez o replace pois se tivesse feito o 'splint()' antes o método 'replace()' não aceitaria o objeto com #splint
+#palavras= novo_texto.split()
+#
+#print(palavras)
+#
+#contage_palavras= {}
+#
+#
+#for palavra in palavras:
+    #if palavra in contage_palavras:
+        #contage_palavras[palavra]= +1
+#else:
+    #contage_palavras[palavra]= 1        
+
+
+import time
+
+cond= True
+
+while cond:
+    print('Execulte miha ETL')
+    time.sleep(5)
